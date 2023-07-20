86925bfc835678f06e66c027dd1a81ed0767b7c0
Xarristo <115558698+Xarristo@users.noreply.github.com>
2023-07-20 13:14:59 +0200

    added colors

--- ATAPHtmlReport/Tests/Get-ColorValue.Test.ps1
+++ ATAPHtmlReport/Tests/Get-ColorValue.Test.ps1
@@ -3,16 +3,53 @@
 
 InModuleScope ATAPHtmlReport {
     Describe "Testing Get-ColorValue" {
-            It "Should return true" {
-                $result = Get-ColorValue -FirstValue 4 -SecondValue 4
-                $result | Should -Be 1
+            It "Should return hundred" {
+                $result = Get-ColorValue -FirstValue 10 -SecondValue 10
+                $result | Should -Be "hundred"
             }
-        }
-
-        Context "When comparing different integers" {
-            It "Should return false" {
-                $result = Get-ColorValue -FirstValue 3 -SecondValue 7
-                $result | Should -Be 0
+            It "Should return ninety" {
+                $result = Get-ColorValue -FirstValue 9 -SecondValue 10
+                $result | Should -Be "ninety"
             }
-        }
+            It "Should return eighty" {
+                $result = Get-ColorValue -FirstValue 8 -SecondValue 10
+                $result | Should -Be "eighty"
+            }
+            It "Should return seventy" {
+                $result = Get-ColorValue -FirstValue 7 -SecondValue 10
+                $result | Should -Be "seventy"
+            }
+            It "Should return sixty" {
+                $result = Get-ColorValue -FirstValue 6 -SecondValue 10
+                $result | Should -Be "sixty"
+            }
+            It "Should return fifty" {
+                $result = Get-ColorValue -FirstValue 5 -SecondValue 10
+                $result | Should -Be "fifty"
+            }
+            It "Should return fourty" {
+                $result = Get-ColorValue -FirstValue 4 -SecondValue 10
+                $result | Should -Be "fourty"
+            }
+            It "Should return thirty" {
+                $result = Get-ColorValue -FirstValue 3 -SecondValue 10
+                $result | Should -Be "thirty"
+            }
+            It "Should return twenty" {
+                $result = Get-ColorValue -FirstValue 2 -SecondValue 10
+                $result | Should -Be "twenty"
+            }
+            It "Should return ten" {
+                $result = Get-ColorValue -FirstValue 1 -SecondValue 10
+                $result | Should -Be "ten"
+            }
+            It "Should return zero" {
+                $result = Get-ColorValue -FirstValue 0 -SecondValue 10
+                $result | Should -Be "zero"
+            }
+            It "Should return grey" {
+                $result = Get-ColorValue -FirstValue 0 -SecondValue 0
+                $result | Should -Be "grey"
+            }
+    }
 }
